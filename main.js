10aa69c2ab71057716061a91203c68796710f351
ehoversten <ehoversten@gmail.com>
2019-03-01 16:17:06 -0500

    added transitions

--- main.js
+++ main.js
@@ -4,22 +4,22 @@ const right = document.querySelector(".right");
 const container = document.querySelectory(".container");
 
 // Add EventListeners (ES5 style)
-left.addEventListener('mouseenter', function() {
+left.addEventListener("mouseenter", () => {
     // add a class of 'hover' to event listener
-    container.classList.add('hover-left');
+    container.classList.add("hover-left");
 });
 
 // Remove Listener (ES5 style)
-left.addEventListener('mouseleave', function() {
-    container.classList.remove('hover-left');
+left.addEventListener("mouseleave", () => {
+    container.classList.remove("hover-left");
 });
 
 // Add EventListener (ES6 style, arrow function)
-right.addEventListener('mouseenter', () => {
-    container.classList.add('hover-right');
+right.addEventListener("mouseenter", () => {
+    container.classList.add("hover-right");
 });
 
 // Remove Listener (ES6 style ,arrow function)
-right.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-riht');
+right.addEventListener("mouseleave", () => {
+    container.classList.remove("hover-right");
 });
