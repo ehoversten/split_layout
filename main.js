dbf480d181bf94acf5093523814758e79bd022bc
ehoversten <ehoversten@gmail.com>
2019-03-01 19:34:40 -0500

    added media queries

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // Setup our Variables
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
-const container = document.querySelectory(".container");
+const container = document.querySelector(".container");
 
 // Add EventListeners (ES5 style)
-left.addEventListener("mouseenter", () => {
+left.addEventListener("mouseenter", function() {
     // add a class of 'hover' to event listener
     container.classList.add("hover-left");
 });
 
 // Remove Listener (ES5 style)
-left.addEventListener("mouseleave", () => {
+left.addEventListener("mouseleave", function() {
     container.classList.remove("hover-left");
 });
 
